f83b8e60d3c7e5bc4e30c04de352dd1946541442
max-xd <ankitgaikwad439@gmail.com>
2021-11-28 17:07:52 +0530

    Started Contributed to Open Source

---
+++ app/src/main/java/com/example/marwiki/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.marwiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    final private static int SPLASH_TIME_OUT=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent MainIntent = new Intent(MainActivity.this,dashboard.class);
+                startActivity(MainIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
